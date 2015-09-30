d7b6bc010abaa7a3bd99ee978792904b3bb74ba4
Rachel Kawula <rkawula@gmail.com>
2015-09-29 20:03:25 -0700

    Revert "Trying to make a custom Writable key (Text pair)."
    This reverts commit b62364daa152335c5b25669a19ca77338b3ae832.
    
    Restarting with basic word pairs before writing custom writable/serializable keys.

--- WordPairCount.java
+++ WordPairCount.java
@@ -1,7 +1,5 @@
 import java.io.IOException;
 import java.util.*;
-import java.io.DataInput;
-import java.io.DataOutput;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.conf.*;
@@ -11,7 +9,6 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-
 
 public class WordPairCount {
 
@@ -23,9 +20,9 @@ public class WordPairCount {
             StringTokenizer tokenizer = new StringTokenizer(line);
             String previousToken = "";
             while (tokenizer.hasMoreTokens()) {
-                String currentToken = tokenizer.nextToken();
+                String currentToken = tokenizer.nextToken());
                 if (previousToken != "") {
-                    WordPairWritable wpw = new WordPairWritable(new Text(previousToken), new Text(currentToken));
+                    WordPairWritable wpw = new WordPairWritable(previousToken, currentToken);
                     context.write(wpw, one);
                 }
                 previousToken = currentToken;
@@ -44,36 +41,14 @@ public class WordPairCount {
         }
     }
 
-    public static class WordPairWritable implements Writable {
+    public static class WordPairWritable extends ArrayWritable {
 
         Text[] words = new Text[2];
 
-        public WordPairWritable() {
-            words[0] = new Text();
-            words[1] = new Text();
-        }
-
         public WordPairWritable(Text first, Text second) {
+            super(WordPairWritable.class);
             words[0] = first;
             words[1] = second;
-        }
-
-        public void write(DataOutput dataOutput) throws IOException {
-            words[0].write(dataOutput);
-            words[1].write(dataOutput);
-        }
- 
-        public void readFields(DataInput dataInput) throws IOException {
-            words[0].readFields(dataInput);
-            words[1].readFields(dataInput);
-        }
-
-        public Text getFirstWord() {
-            return words[0];
-        }
-
-        public Text getSecondWord() {
-            return words[1];
         }
 
     }
